3ce9ebf708fcc000c3648f6d210f988f4e7d7ee8
Luke Wurl <lwurl@nd.edu>
2018-05-29 15:24:30 -0400

    Add sumbit handler that pushes username onto history

--- src/Github.js
+++ src/Github.js
@@ -10,7 +10,12 @@ class Github extends Component {
     handleChange = (ev) => {
         this.setState({
             username: ev.target.value
-        })
+        });
+    }
+
+    handleSubmit = (ev) => {
+        ev.preventDefault();
+        this.props.history.push(`/github/${this.state.username}`);
     }
 
     render() {
@@ -21,7 +26,7 @@ class Github extends Component {
                     alt="GitHub"
                     className="logo"
                 />
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <input 
                             type="text"
