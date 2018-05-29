d7598d0e579b055a15a930b50954dea4967a2eb6
Luke Wurl <lwurl@nd.edu>
2018-05-29 15:55:23 -0400

    GithubUser info with API

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
 class GithubUser extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: {}
+        };
+
+        this.fetchUserData();
+    }
+
+    componentWillReceiveProps = (newProps) => {
+        const locationChanged = newProps.location !== this.props.location;
+        if (locationChanged) {
+            this.fetchUserData(newProps);
+        }
+    }
+
+    fetchUserData = (props) => {
+        fetch(`https://api.github.com/users/${this.props.match.params.username}`)
+            .then(response => response.json())
+            .then(user => this.setState({ user: user }))
+    }
+
     render() {
+        const { user } = this.state
         return (
             <div className="GithubUser">
-                <h1>User: {this.props.match.params.username}</h1>
+                <img src={user.avatar_url} alt="" />
+                <h2>{user.login}</h2>
+                <h3>followers: {user.followers}</h3>
+                <h3>following: {user.following}</h3>
+                <h3>location: {user.location}</h3>
+                <a href={user.html_url} target="_blank">Link to {user.login}'s profile</a>
             </div>
         );
     }
